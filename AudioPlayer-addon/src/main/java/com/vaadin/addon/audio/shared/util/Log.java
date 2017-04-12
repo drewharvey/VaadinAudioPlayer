7090371da9b80a69e7e5b8821be670e0c88aa07a
Patrik Lindström <patrik@vaadin.com>
2017-04-12 12:48:03 +0300

    Improve Log

--- AudioPlayer-addon/src/main/java/com/vaadin/addon/audio/shared/util/Log.java
+++ AudioPlayer-addon/src/main/java/com/vaadin/addon/audio/shared/util/Log.java
@@ -1,4 +1,4 @@
-package com.vaadin.addon.audio.client.util;
+package com.vaadin.addon.audio.shared.util;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -9,11 +9,16 @@ import java.util.logging.Logger;
  * Simple mostly devtime helper to get around having to actually
  * declare a logger for every class...
  * 
- *  XXX: of course dynamic name resolution doesn't work in GWT... *sigh*
- *  TODO: figure out some better way to log stuff. Perhaps a direct call to console.log? Always log a warning? What?
+ * TODO: improve logging further.
  */
 public class Log {
 	private static final Map<String, Logger> loggers = new HashMap<String, Logger>();
+	
+	private static <T> String getInstanceName(T instance) {
+		String name = instance.getClass().getSimpleName();
+		if(!name.isEmpty()) return name;
+		return instance.toString();
+	}
 	
 	private static Logger getLogger(String cls) {
 		if(loggers.containsKey(cls)) {
@@ -24,15 +29,32 @@ public class Log {
 		return logger;
 	}
 	
+	private static void logMessage(String owner, String message, Level logLevel) {
+		Logger l = getLogger(owner);
+		l.log(logLevel, "[" + owner + "] " + message);
+	}
+	
+	public static <T> void message(Class<T> cls, String message) {
+		logMessage(cls.getSimpleName(), message, Level.INFO);
+	}
+	
 	public static <T> void message(T instance, String message) {
-		getLogger(instance.toString()).log(Level.INFO, message + " [REMOVEME]");
+		logMessage(getInstanceName(instance), message, Level.INFO);
+	}
+
+	public static <T> void warning(Class<T> cls, String message) {
+		logMessage(cls.getSimpleName(), message, Level.WARNING);
 	}
 	
 	public static <T> void warning(T instance, String message) {
-		getLogger(instance.toString()).log(Level.WARNING, message);
+		logMessage(getInstanceName(instance), message, Level.WARNING);
+	}
+	
+	public static <T> void error(Class<T> cls, String message) {
+		logMessage(cls.getSimpleName(), message, Level.SEVERE);
 	}
 	
 	public static <T> void error(T instance, String message) {
-		getLogger(instance.toString()).log(Level.SEVERE, message);
+		logMessage(getInstanceName(instance), message, Level.SEVERE);
 	}
 }
