747aa44cee1967375b0aadf326ca454479e9592a
Patrik Lindström <patrik@vaadin.com>
2017-04-13 01:20:58 +0300

    Add AudioDestinationNode class

---
+++ AudioPlayer-addon/src/main/java/com/vaadin/addon/audio/client/webaudio/AudioDestinationNode.java
@@ -0,0 +1,9 @@
+package com.vaadin.addon.audio.client.webaudio;
+
+public class AudioDestinationNode extends AudioNode {
+
+	protected AudioDestinationNode(elemental.html.AudioNode nativeNode) {
+		super(nativeNode);
+	}
+
+}
