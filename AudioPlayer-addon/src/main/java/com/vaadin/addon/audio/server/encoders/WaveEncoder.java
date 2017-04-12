a8770111a4c4854ff9c8c738ea6feb81523063a3
Patrik Lindström <patrik@vaadin.com>
2017-04-12 18:06:37 +0300

    Implement WaveEncoder

--- AudioPlayer-addon/src/main/java/com/vaadin/addon/audio/server/encoders/WaveEncoder.java
+++ AudioPlayer-addon/src/main/java/com/vaadin/addon/audio/server/encoders/WaveEncoder.java
@@ -4,6 +4,7 @@ import java.nio.ByteBuffer;
 
 import com.vaadin.addon.audio.server.Encoder;
 import com.vaadin.addon.audio.shared.PCMFormat;
+import com.vaadin.addon.audio.shared.util.WaveUtil;
 
 /**
  * PCM-to-MSWave encoder.
@@ -24,12 +25,21 @@ public class WaveEncoder extends Encoder {
 	public byte[] encode(int from_offset, int length) {
 		
 		PCMFormat infmt = getInputFormat();
-		PCMFormat outfmt = outputFormat == null ? infmt : outputFormat;
-		
+
 		// TODO: allow different input and output formats - we currently do no conversion
-				
+		//PCMFormat outfmt = outputFormat == null ? infmt : outputFormat;
+		PCMFormat outfmt = infmt;
 		
-		return null;
+		int dataLength = infmt.getSampleSize() * length;
+		
+		ByteBuffer in = getInputBuffer();
+		ByteBuffer out = ByteBuffer.allocate(dataLength + WaveUtil.getDataStartOffset(null));
+		
+		out.put(WaveUtil.generateHeader(outfmt, length));
+		out.put(in.array());
+		
+		return out.array();
+		
 	}
 
 }
